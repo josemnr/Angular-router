3b9dc5c3bd435eb1ccae660fd511286217527d8f
josemnr <maninari97@gmail.com>
2022-09-09 12:15:23 -0500

    Add Website module

---
+++ src/app/website/website.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { WebsiteRoutingModule } from './website-routing.module';
+
+import { SwiperModule } from 'swiper/angular';
+
+import { ImgComponent } from './components/img/img.component';
+import { LayoutComponent } from './components/layout/layout.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ProductComponent } from './components/product/product.component';
+import { ProductsComponent } from './components/products/products.component';
+
+import { HomeComponent } from './pages/home/home.component';
+import { CategoryComponent } from './pages/category/category.component';
+import { MyCartComponent } from './pages/my-cart/my-cart.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { RecoveryComponent } from './pages/recovery/recovery.component';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { ProductDetailComponent } from './pages/product-detail/product-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    ImgComponent,
+    LayoutComponent,
+    NavComponent,
+    ProductComponent,
+    ProductsComponent,
+    HomeComponent,
+    CategoryComponent,
+    MyCartComponent,
+    LoginComponent,
+    RegisterComponent,
+    RecoveryComponent,
+    ProfileComponent,
+    ProductsComponent,
+    ProductDetailComponent
+  ],
+  imports: [
+    CommonModule,
+    SwiperModule,
+    WebsiteRoutingModule
+  ]
+})
+export class WebsiteModule { }
