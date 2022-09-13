ff808a257c4aaf17ebbdea640b52e5c6a95cfa7f
josemnr <maninari97@gmail.com>
2022-09-12 20:16:39 -0500

    Add auth guard with redirect

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs';
 
@@ -10,14 +11,18 @@ import { TokenService } from '../services/token.service';
 export class AuthGuard implements CanActivate {
 
     constructor(
-        private tokenService: TokenService
+        private tokenService: TokenService,
+        private router: Router
     ) {}
     
     canActivate(
         route: ActivatedRouteSnapshot, 
         state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
         const token = this.tokenService.getToken();
-        return token ? true : false;
+        if(!token) {
+            this.router.navigate(['/home']);
+            return false;
+        }
+        return true;
     }
-
 }
